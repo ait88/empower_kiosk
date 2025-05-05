4f2e0f677ac7305f2bfaff28c2207b45cbc248e3
pm <pm@noreply.git.aitdev.au>
2025-05-05 11:22:58 +0000

    Update chromium.sh

--- chromium.sh
+++ chromium.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-source /home/kiosk/.kiosk-config
+source /home/kiosk/.kiosk-config        
 LOG_FILE="/tmp/chromium.log"
 
-# Make sure DISPLAY is set (Openbox gives us this)
-export DISPLAY=:0
+export DISPLAY=:0                       
 
-# Start loop to re-launch chromium if it's killed or crashes
 while true; do
   echo "[*] Launching Chromium..." | tee -a "$LOG_FILE"
+
   /usr/bin/chromium-browser \
     --no-first-run \
     --kiosk \
@@ -17,13 +16,12 @@ while true; do
     --overscroll-history-navigation=0 \
     --noerrdialogs \
     --disable-pinch \
-    --use-gl=desktop \
     --disable-session-crashed-bubble \
     --disable-component-update \
     --disable-extensions \
     "$portal_url" 2>&1 | tee -a "$LOG_FILE"
 
-  echo "[!] Chromium crashed or was closed. Rebooting..." | tee -a "$LOG_FILE"
+  echo "[!] Chromium crashed or closed - rebooting." | tee -a "$LOG_FILE"
   sleep 2
   sudo reboot
 done
