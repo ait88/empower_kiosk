d05e8de58f6436a469e2dc993db3fd0bda9e578b
pm <pm@noreply.git.aitdev.au>
2025-05-05 10:47:03 +0000

    Update chromium.sh

--- chromium.sh
+++ chromium.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+LOG_FILE="/tmp/chromium.log"
 
+# Make sure DISPLAY is set (Openbox gives us this)
+export DISPLAY=:0
+
+# Start loop to re-launch chromium if it's killed or crashes
 while true; do
-  openbox-session &
-  chromium-browser \
+  echo "[*] Launching Chromium..." | tee -a "$LOG_FILE"
+  /usr/bin/chromium-browser \
     --no-first-run \
     --kiosk \
     --disable-translate \
@@ -11,8 +16,14 @@ while true; do
     --disable-features=TranslateUI \
     --overscroll-history-navigation=0 \
     --noerrdialogs \
-    "$portal_url"
-  echo "[!] Chromium crashed or was closed. Rebooting..."
+    --disable-pinch \
+    --use-gl=desktop \
+    --disable-session-crashed-bubble \
+    --disable-component-update \
+    --disable-extensions \
+    "$portal_url" 2>&1 | tee -a "$LOG_FILE"
+
+  echo "[!] Chromium crashed or was closed. Rebooting..." | tee -a "$LOG_FILE"
   sleep 2
   sudo reboot
 done
